cfba6a2e21443e67e7943f4adb85b6a4ac125cc3
M0rty <martin.hager@outlook.com>
2026-05-11 13:42:20 +0200

    check login

---
+++ bin/login.php
@@ -0,0 +1,42 @@
+<?php
+
+
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    header("Location: /public/login.php");
+    exit;
+}
+
+$email = $_POST["email"] ?? null;
+$passwort = $_POST["passwort"] ?? null;
+
+echo ($email);
+echo ($passwort);
+
+$rightEmail = "hager@mogic.de";
+$rightPasswort = "123";
+
+// die();
+
+if ($email == $rightEmail && $passwort == $rightPasswort) {
+
+    header("Location: /public/inhaltsverzeichniss.html");
+    exit;
+
+} else {
+
+    // Login falsch
+    header("Location: /public/login.php");
+    exit;
+}
+
+
+// build simple login check
+
+
+// pw and username can be hardcoded
+
+// when login
+//  ok => redirect to inhalt
+
+//  not ok => // redirect to login with message
+
---
+++ public/login.php
@@ -0,0 +1,25 @@
+<html>
+
+<head>
+    <link rel="stylesheet" href="/style/style.css">
+    <title>Hager games</title>
+</head>
+
+<body>
+    <h1>LOGIN</h1>
+    <div class="container">
+        <form id="login" method="POST" action="/bin/login.php">
+            <input name="email" id="email" type="email" placeholder="Email">
+            <br><br>
+            <input name="passwort" id="passwort" type="password" placeholder="Passwort">
+            <br><br>
+            <input type="submit" value="Bestätigen" />
+            <!-- <button type="submit">Bestätigen</button> -->
+        </form>
+    </div>
+
+    <script src="login.js"></script>
+
+</body>
+
+</html>
